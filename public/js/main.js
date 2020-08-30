295f188bdc696a6ea9ff0604688dc4701b0ee983
Jose Mauricio <jmauricio.a.ferreira@gmail.com>
2020-08-30 18:35:58 -0300

    criando animacao placar deslizante

--- public/js/main.js
+++ public/js/main.js
@@ -66,27 +66,6 @@ function inicializaMarcadores() {
     });
 }
 
-function inserePlacar() {
-    var corpoTabela = $(".placar").find("tbody");
-    var usuario = "Usuario";
-    var numPalavras = $("#contador-palavras").text();
-
-    var botaoRemover = "<a href='#'><i class='material-icons'>delete</i></a>"
-
-    var linha = "<tr>" +
-                    "<td>" + usuario + "</td>" +
-                    "<td>" + numPalavras + "</td>" +
-                    "<td>" + botaoRemover + "</td>" +
-                "</tr>";
-    
-    corpoTabela.prepend(linha);
-}
-
-$(".botao-remover").click(function(event){
-    event.preventDefault();
-    $(this).parent().parent().remove();
-
-});
 
 function reiniciaJogo() {
     campo.attr("disabled", false);
