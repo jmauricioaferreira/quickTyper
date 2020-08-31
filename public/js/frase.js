f3eec9e5c38d7183e0af9ee4168cddd15fca2cc9
Jose Mauricio <jmauricio.a.ferreira@gmail.com>
2020-08-31 20:31:10 -0300

    Criando a função para escolher uma frase pelo id

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,7 +1,20 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria() {
-    $.get("http://localhost:3000/frases", trocaFraseAleatoria);
+    $("#spinner").toggle();
+
+    $.get("http://localhost:3000/frases", trocaFraseAleatoria)
+    .fail(function() {
+        $("#erro").toggle();
+        setTimeout(function () {
+            $("#erro").toggle();
+        }, 2500)
+    })
+
+    .always(function() {
+        $("#spinner").toggle()
+    })
 
 }
 
@@ -10,4 +23,32 @@ function trocaFraseAleatoria(data) {
     var numeroAleatorio = Math.floor(Math.random() * data.length);
 
     frase.text(data[numeroAleatorio].texto);
+
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data[numeroAleatorio].tempo);
+
+}
+
+function buscaFrase() {
+    $("#spinner").toggle();
+    var fraseId= $("#frase-id").val();
+    var dados = { id: fraseId};
+    $.get("http://localhost:3000/frases", dados, trocaFrase)
+    .fail(function() {
+        $("#erro").toggle();
+        setTimeout(function () {
+            $("#erro").toggle();
+        }, 2500)
+    })
+    .always(function() {
+        $("#spinner").toggle()
+    });
+
+}
+
+function trocaFrase (data) {
+   var frase = $(".frase");
+   frase.text(data.texto);
+   atualizaTamanhoFrase();
+   atualizaTempoInicial();
 }
